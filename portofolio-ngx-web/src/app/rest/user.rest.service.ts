496e13843ec84604ba8f4347eea45cee8da314e8
ding <zuosheng.ding@adesso.de>
2024-08-11 14:39:54 +0200

    added environments and rest-services

--- portofolio-ngx-web/src/app/rest/user.rest.service.ts
+++ portofolio-ngx-web/src/app/rest/user.rest.service.ts
@@ -1,10 +1,30 @@
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
+import {Project, User} from "../core/model";
+import {Observable} from "rxjs";
+import {environment} from "../../environments/environment";
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserRestService {
-  constructor(private httpClient: HttpClient) {}
+  apiUrl = environment.aipEndpoints.portofolioBaseUrl;
+
+  constructor(private http: HttpClient) {}
+
+  public registerUser(user: User): Observable<void> {
+    const headers = { 'Content-Type': 'application/json' };
+    return this.http.post<void>(`${this.apiUrl}/api/user`, user, {headers});
+  }
+
+  public loadUser(id: string): Observable<User> {
+    return this.http.get<User>(`${this.apiUrl}/api/user/${id}`);
+  }
+
+  public saveProject(id: string, project: Project): Observable<void> {
+    const headers = { 'Content-Type': 'application/json' };
+    return this.http.post<void>(`${this.apiUrl}/api/user/${id}/projects`, project, {headers});
+  }
 
 }
---
+++ portofolio-ngx-web/src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  aipEndpoints: {
+    portofolioBaseUrl: 'http_//localhost:8081',
+  }
+};
